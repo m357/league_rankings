c67850543d177b42a1f2664767dfc9d649a5a3ab
Madeleine Clark <madeleineclark.0@gmail.com>
2017-03-15 14:12:37 -0400

    Consolidated into Standings.rb

--- standings.rb
+++ standings.rb
@@ -2,12 +2,15 @@ require 'pry'
 require_relative './teams.rb'
 
 
-
-def compare_scores()
+def compare_scores
   # file = File.open('sample-input.txt', 'r')
+  puts "League standings calculator"
+  puts "Please enter your league's results:"
+  input = gets.chomp
   # league_results = file.read.split("\n")
+  file = File.open(input, 'r').read.split("\n")
   results = []
-  league_results.each do |t|
+  file.each do |t|
     Team.read_game_score(t)
   end
   Team.output_rankings
