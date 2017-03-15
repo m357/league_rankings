72b73d2df5ae1126b05fd2bf079c01099229394c
Madeleine Clark <madeleineclark.0@gmail.com>
2017-03-15 14:21:24 -0400

    take a look

--- runner.rb
+++ runner.rb
@@ -2,4 +2,15 @@ require 'pry'
 require_relative './teams.rb'
 require_relative './standings.rb'
 
-compare_scores
+require 'pry'
+require_relative './teams.rb'
+require_relative './standings.rb'
+
+  def run_all()
+    puts "League standings calculator"
+    puts "Please enter your league's results:"
+    input = gets.chomp
+    file = File.open(input, 'r').read.split("\n")
+    compare_scores(file)
+  end
+run_all
