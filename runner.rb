01903d228f9344798c063f5e254d4c4b847357bd
Madeleine Clark <madeleineclark.0@gmail.com>
2017-03-16 09:55:24 -0400

    removes pry

--- runner.rb
+++ runner.rb
@@ -1,8 +1,8 @@
 require_relative './standings.rb'
 
 def run_all()
-  puts "League standings calculator"
-  puts "Please enter your league's results:"
+  puts "Welcome to the league standings calculator!"
+  puts "Please enter the filepath where your league's results live:"
   input = gets.chomp
   file = File.open(input, 'r').read.split("\n")
   compare_scores(file)
